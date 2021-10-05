3e73cf8a64a81e02a6cb0d826420d8b04d8a2848
Zuzana Pelechová <zpelechova@gmail.com>
2021-10-05 08:51:08 +0200

    first try

--- main.js
+++ main.js
@@ -1,36 +1,76 @@
 import Apify from 'apify';
-import fs from 'fs';
-
+import { gotScraping } from 'got-scraping';
 import { config } from 'dotenv';
-import * as lib from './lib.js';
-
 config();
 
-const shopName = 'aab';
-
 Apify.main(async () => {
-    console.log(process.env.KEBOOLA_TOKEN);
+    const shopName = await Apify.getInput();
 
-    const id1 = await lib.getOrCreateTransformation(shopName, '_01_unification');
-    const id2 = await lib.getOrCreateTransformation(shopName, '_02_enriched');
-    const id3 = await lib.getOrCreateTransformation(shopName, '_03_upload');
+    const getUrl =
+        'https://connection.eu-central-1.keboola.com/v2/storage/components/transformation/configs'
+    const getMethod = 'GET'
+    const getHeaders = {
+        'content-type': 'application/json',
+        'x-storageapi-token': process.env.KEBOOLA_TOKEN
+    }
 
-    await lib.updateTransformation(
-        id1,
-        `in.c-black-friday.${shopName}`,
-        `out.c-0-${shopName}.${shopName}_unified`,
-        fs.readFileSync('./01_unification.sql', 'utf-8'),
-    );
-    await lib.updateTransformation(
-        id2,
-        `out.c-0-${shopName}.${shopName}_unified`,
-        `out.c-0-${shopName}.${shopName}_enriched`,
-        `TODO`,
-    );
-    await lib.updateTransformation(
-        id3,
-        `out.c-0-${shopName}.${shopName}_enriched`,
-        `out.c-0-${shopName}.${shopName}_connected`,
-        `TODO`,
-    );
-});
+    const { body: getBody } = await gotScraping({
+        useHeaderGenerator: false,
+        url: getUrl,
+        method: getMethod,
+        headers: getHeaders
+    })
+
+    // console.dir(JSON.parse(body));
+    const data = JSON.parse(getBody);
+    const bucketData = data.find(i => i.name === `#0 ${shopName.shopName}`);
+    const bucketId = bucketData.id;
+
+    // check if tranformation dashboard already exists, if yes, return id
+    const transformationData = bucketData.rows.find(i => i.name === `dashboard`)
+    if (transformationData) {
+        Apify.pushData({
+            shop: shopName,
+            bucketId,
+            transformationId: transformationData.id
+        })
+    
+    }
+
+    // Otherwise create
+    const postUrl = `https://connection.eu-central-1.keboola.com/v2/storage/components/transformation/configs/${bucketId}/rows`
+    const postMethod = 'POST'
+    const postHeaders = {
+        'content-type': 'application/x-www-form-urlencoded',
+        'x-storageapi-token':
+            '395-17125-bvfgOWfYTe9KR14ab4krGphGOqaEJWSqXGUFP627'
+    }
+    const postFormData = {
+        name: 'dashboard',
+        changeDescription: 'Create transformation dashboard',
+        configuration: JSON.stringify({
+            backend: 'snowflake',
+            type: 'simple',
+            queries: [
+                '-- Here goes querry'
+            ]
+        })
+    }
+
+    const { body: postBody } = await gotScraping({
+        useHeaderGenerator: false,
+        url: postUrl,
+        method: postMethod,
+        headers: postHeaders,
+        form: postFormData
+    })
+
+
+
+    Apify.pushData({
+        shop: shopName,
+        bucketId,
+        transformationId: JSON.parse(postBody).id
+    })
+
+})
